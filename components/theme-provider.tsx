53bbf922c76819cd0ab22deb15da70dd21d18d5d
TissuGalon <parzivalxdd@gmail.com>
2026-06-11 00:03:25 +0700

    first commit

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -3,6 +3,20 @@
 import * as React from "react"
 import { ThemeProvider as NextThemesProvider, useTheme } from "next-themes"
 
+// Suppress React 19 console error for next-themes script tag in development
+if (typeof window !== "undefined" && process.env.NODE_ENV === "development") {
+  const orig = console.error
+  console.error = (...args: any[]) => {
+    if (
+      typeof args[0] === "string" &&
+      args[0].includes("Encountered a script tag while rendering React component")
+    ) {
+      return
+    }
+    orig.apply(console, args)
+  }
+}
+
 function ThemeProvider({
   children,
   ...props
